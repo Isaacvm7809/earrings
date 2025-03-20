a5a69973abaf49f203be5f59eb062ed118e1b9e4
Isaac M. Vásquez <isaacvm7809@gmail.com>
2025-03-20 01:13:48 -0600

    Coloured version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardModule, MatIconModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'earrings';
+
+openYouTube(){
+  const url = 'https://youtu.be/lL_wkVcU7e0?si=-1NigNvvXHLEM4Ry';
+    window.open(url, '_blank');
+  
+}
+
 }
